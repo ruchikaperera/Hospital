28b8c49d89ec0fd92445455e425993dd0134c90c
Ruchika Madhubhashitha <madubashitharuchika@yahoo.com>
2018-02-26 21:05:18 +0530

    aa

---
+++ Hospital/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    public class Class
+    {
+        public string ruchika;
+    }
+}
